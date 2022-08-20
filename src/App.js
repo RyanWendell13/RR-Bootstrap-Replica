3cadb3cea8d1ec05051cc726de03c84bcec14c9f
Ryan Wendell <Ryan.wendell@me.com>
2022-08-20 00:10:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from "react"
+import TopBar from './components/TopBar';
+import StoreListing from './components/StoreListing';
+import { Card } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar/>
+      <Card.Img variant="top" src="https://www.instacart.com/image-server/x428/www.instacart.com/assets/homepage/homepage_background_full_m3_cropped-8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg"/>
+      
+
+      <StoreListing/>
     </div>
   );
 }
